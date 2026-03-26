2e8224e2c13e924fd104029af9dd2fef18063647
John Manchego <jmanchego+oxxogh@baufest.com>
2026-03-26 09:31:55 -0500

    add BD

--- src/main 2/java/com/jpomm/schedulerbase/transmission/facade/TransmissionFacade.java
+++
@@ -1,9 +0,0 @@
-package com.jpomm.schedulerbase.transmission.facade;
-
-import com.jpomm.schedulerbase.transmission.dto.TransmissionResponse;
-
-import java.util.List;
-
-public interface TransmissionFacade {
-    List<TransmissionResponse> listTransmissions();
-}
--- src/main 2/java/com/jpomm/schedulerbase/transmission/service/TransmissionQueryService.java
+++
@@ -1,9 +0,0 @@
-package com.jpomm.schedulerbase.transmission.service;
-
-import com.jpomm.schedulerbase.transmission.dto.TransmissionResponse;
-
-import java.util.List;
-
-public interface TransmissionQueryService {
-    List<TransmissionResponse> listTransmissions();
-}
